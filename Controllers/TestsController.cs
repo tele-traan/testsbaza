4c4db667f0c1ec3c007747869ae233c80f07efd2
Timur <timurturovets@mail.ru>
2022-02-15 23:46:26 +0300

    Вторая часть контроллера тестов

--- Controllers/TestsController.cs
+++ Controllers/TestsController.cs
@@ -37,7 +37,7 @@ namespace TestsBaza.Controllers
         public IActionResult GetTest([FromQuery][FromRoute]int testId)
         {
             Test? test = _testsRepo.GetTest(testId);
-            if (test is null) return NotFound(new { msg = $"Тест с идентификатором {testId} не существует"});
+            if (test is null) return NotFound(new { msg = $"Теста с идентификатором {testId} не существует"});
             return Json(test!);
         }
 
@@ -45,7 +45,7 @@ namespace TestsBaza.Controllers
         public IActionResult GetTest([FromBody][FromForm]string testName)
         {
             Test? test = _testsRepo.GetTest(testName);
-            if (test is null) return NotFound(new { msg = $"Тест с именем {testName} не существует" });
+            if (test is null) return NotFound(new { msg = $"Теста с именем {testName} не существует" });
             return Json(test!);
         }
         [HttpPost("createtest")]
@@ -71,10 +71,12 @@ namespace TestsBaza.Controllers
         }
 
         [HttpPost("updatetest")]
-        public IActionResult UpdateTest([FromBody][FromForm] UpdateTestRequestModel model)
+        public async Task<IActionResult> UpdateTest([FromBody][FromForm] UpdateTestRequestModel model)
         {
+            User? creator = await _userManager.FindByNameAsync(User.Identity!.Name!);
             Test? test = _testsRepo.GetTest(model.TestId);
-            if (test is null) return NotFound(new { message = $"Тест с идентификатором {model.TestId} не существует" });
+            if (test is null) return NotFound(new { message = $"Теста с идентификатором {model.TestId} не существует" });
+            if (creator is null || !test.Creator.Equals(creator)) return Unauthorized();
             test.TestName = model.NewTestName ?? test!.TestName;
             test.Questions = test.Questions.Concat(model.NewQuestions.Select(q=>new Question
             {
@@ -83,6 +85,17 @@ namespace TestsBaza.Controllers
                 Test = test
             }));
             _testsRepo.UpdateTest(test);
+            return Ok();
+        }
+
+        [HttpPost("removetest")]
+        public async Task<IActionResult> RemoveTest([FromBody][FromForm] int testId)
+        {
+            User? creator = await _userManager.FindByNameAsync(User.Identity!.Name!);
+            Test? test = _testsRepo.GetTest(testId);
+            if (test is null) return NotFound(new { message = $"Теста с идентификатором {testId} не существует" });
+            if (creator is null || !test.Creator.Equals(creator)) return Unauthorized();
+            _testsRepo.RemoveTest(test);
             return Ok();
         }
     }
